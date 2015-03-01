2777e2c9c352a03a1dfb3e79352ce207ae82ab1c
Brook <chenli_better@163.com>
2015-03-01 09:03:56 +0800

    Initial Commit

---
+++ easyFin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  easyFin
+//
+//  Created by Brook on 15/3/1.
+//  Copyright (c) 2015年 Brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ easyFin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  easyFin
+//
+//  Created by Brook on 15/3/1.
+//  Copyright (c) 2015年 Brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
